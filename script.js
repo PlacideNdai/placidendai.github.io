1854488c8e9f21dad10947980ec87bdd49f44efc
Placide <75864037+PlacideNdai@users.noreply.github.com>
2022-11-18 22:20:08 -0600

    js intro

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-window.onload = ()=>{
-    alert( "The site may appear broken on certain phones.")
+// window.onload = ()=>{
+//     alert( "The site may appear broken on certain phones.")
+// }
+
+let greet = document.getElementById('greet');
+
+
+function greetings(){
+    let now = new Date();
+    let hours = now.getHours();
+
+
+    if(hours >= 0 && hours <= 12){
+        greet.innerHTML = ("Good Morning!")
+    } else if(hours >= 12 && hours <= 18){
+        greet.innerHTML = ("Good Afternoon!")
+    } else if(hours >= 18){
+        greet.innerHTML = ("Good Evening!")
+    }
 }
+greetings();
+
+// smiles changer
+function secondEmote(){
+    let smile = document.getElementById('smile');
+    let emotearray = ["😃","😄","😁","😆","😅",'🤣','😂','🙂','🙃','😉','😊','😇','🥰','😍','🤩','😘','😗','☺️','😚','😙','🥲','😋','😛','😜','🤪','😝'];
+
+    randomemote = Math.floor(Math.random() * emotearray.length)
+    smile.innerHTML = (emotearray[randomemote]);
+}
+setInterval(secondEmote,1000)
