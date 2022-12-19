2755fc43889e8686c8d2fa944b79ab83a21d1790
Placide <75864037+PlacideNdai@users.noreply.github.com>
2022-12-18 22:01:27 -0600

    New update

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // window.onload = ()=>{
 //     alert( "The site may appear broken on certain phones.")
 // }
+// home reload to home
+let home = document.getElementById('cohead');
+homereloader = document.querySelector(".name")
 
+homereloader.addEventListener('click',()=>{
+    document.location.reload(1);
+})
+
+//
 let greet = document.getElementById('greet');
 
 
@@ -29,3 +37,104 @@ function secondEmote(){
     smile.innerHTML = (emotearray[randomemote]);
 }
 setInterval(secondEmote,1000)
+
+// all other pages
+let allcontent = document.querySelector(".allcontent");
+
+let chesstab,drawingtab,flowertab,randomtab,friendstab;
+let spanstab = document.querySelectorAll('.tabs')
+//  chess, drawing, friends, flowers, random...
+chesstab = spanstab[0];
+drawingtab = spanstab[1];
+friendstab = spanstab[2];
+flowertab = spanstab[3];
+randomtab = spanstab[4];
+
+let chesspage,drawingpage,flowerpage,randompage,friendspage;
+let pages = document.querySelectorAll('.page')
+chesspage = pages[0];
+drawingpage = pages[1];
+friendspage = pages[2];
+flowerpage = pages[3];
+randompage = pages[4];
+
+pages[0].style.display = 'none';
+pages[1].style.display = 'none';
+pages[2].style.display = 'none';
+pages[3].style.display = 'none';
+pages[4].style.display = 'none';
+
+chesstab.onclick = () =>{
+    if(chesspage.style.display == 'none'){
+        chesspage.style.display = 'block';
+        allcontent.style.display = 'none';
+    pages[1].style.display = 'none';
+    pages[2].style.display = 'none';
+    pages[3].style.display = 'none';
+    pages[4].style.display = 'none';
+    
+    } else{
+        allcontent.style.display = 'block';
+        chesspage.style.display = 'none';
+    }
+}
+
+drawingtab.onclick = () =>{
+    if(drawingpage.style.display == 'none'){
+        drawingpage.style.display = 'block';
+        allcontent.style.display = 'none';
+    pages[0].style.display = 'none';
+    pages[2].style.display = 'none';
+    pages[3].style.display = 'none';
+    pages[4].style.display = 'none';
+    
+    } else{
+        allcontent.style.display = 'block';
+        drawingpage.style.display = 'none';
+    }
+}
+
+friendstab.onclick = () =>{
+    if(friendspage.style.display == 'none'){
+        friendspage.style.display = 'block';
+        allcontent.style.display = 'none';
+    pages[0].style.display = 'none';
+    pages[1].style.display = 'none';
+    pages[3].style.display = 'none';
+    pages[4].style.display = 'none';
+    
+    } else{
+        allcontent.style.display = 'block';
+        friendspage.style.display = 'none';
+    }
+}
+
+flowertab.onclick = () =>{
+    if(flowerpage.style.display == 'none'){
+        flowerpage.style.display = 'block';
+        allcontent.style.display = 'none';
+    pages[0].style.display = 'none';
+    pages[1].style.display = 'none';
+    pages[2].style.display = 'none';
+    pages[4].style.display = 'none';
+    
+    } else{
+        allcontent.style.display = 'block';
+        flowerpage.style.display = 'none';
+    }
+}
+
+randomtab.onclick = () =>{
+    if(randompage.style.display == 'none'){
+        randompage.style.display = 'block';
+        allcontent.style.display = 'none';
+    pages[0].style.display = 'none';
+    pages[1].style.display = 'none';
+    pages[2].style.display = 'none';
+    pages[3].style.display = 'none';
+    
+    } else{
+        allcontent.style.display = 'block';
+        randompage.style.display = 'none';
+    }
+}
