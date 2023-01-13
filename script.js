5767c8952482931de233f5aa2f356418c34ae882
Placide <75864037+PlacideNdai@users.noreply.github.com>
2023-01-13 11:13:06 -0600

    Update

--- script.js
+++ script.js
@@ -28,16 +28,44 @@ setInterval( ()=>{
 
 let btnsong = document.getElementById('song');
 let song = document.getElementById('backso');
+let trad = document.getElementById('trad');
+let tradbtn = document.getElementById('tradbtn');
 
 let playing = false;
+let tradplaying = false;
+
 function play(){
     if(playing == false){
         btnsong.innerHTML = "⏸";
         song.play();
         playing = true;
+            if(tradplaying == true){
+                trad.pause();
+                tradplaying = false;
+                tradbtn.innerHTML = "▶️";
+            }
     }else if(playing == true){
         song.pause();
         playing  = false;
         btnsong.innerHTML = "▶️";
     }
 }
+
+
+function playtrad(){
+    if(tradplaying == false){
+        tradbtn.innerHTML = "⏸";
+        trad.play();
+        tradplaying = true;
+        if(playing == true){
+            song.pause();
+            btnsong.innerHTML = "▶️";
+            playing = false;
+        }
+    }else if(tradplaying == true){
+        trad.pause();
+        tradplaying  = false;
+        tradbtn.innerHTML = "▶️";
+    }   
+}
+
