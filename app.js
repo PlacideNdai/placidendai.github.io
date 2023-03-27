5dac33735be68257e6f43f562f096ce8980160f9
Placide <75864037+PlacideNdai@users.noreply.github.com>
2023-03-27 18:01:47 -0500

    Major update
    Year

---
+++ app.js
@@ -0,0 +1,54 @@
+// js
+let music, blog, hlights, contact;
+music = document.querySelector(".music");
+blog = document.querySelector(".blog");
+hlights = document.querySelector(".hlights");
+contact = document.querySelector(".contacts");
+aboutm = document.getElementById('aboutm');
+// getting data from json.
+fetch("text.json")
+    .then( (res) => 
+         res.json())
+    .then( (data) =>{ 
+        music.innerHTML = data.music;
+        blog.innerHTML = data.blog;
+        hlights.innerHTML = data.hlights;
+        contact.innerHTML = data.contact;
+        aboutm.innerHTML = data.aboutme;
+    })
+
+// blinking dot
+    let number  = 0;
+    let time, now, nowmod, timedis, date;
+    timedis = document.getElementById("timedis");
+    let days = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    wel = document.getElementById('wel');
+    date = document.getElementById('date');
+
+setInterval(()=>{
+
+        if(number == 0){
+            wel.innerHTML = "Welcome";
+            number = 1;
+        } else if(number == 1){
+            wel.innerHTML = "Welcome.";
+            number = 0;
+        }
+// time
+        time = new Date();
+        now = time.toLocaleTimeString();
+        timedis.innerHTML = now;
+//  day
+        nowmod = time.toDateString();
+        date.innerHTML = nowmod;
+},1000);
+
+let words = ["good", "glad","joyful","great","sweet","worm","blessed","strong","best","friendly"]
+
+let reads = 0;
+let ins = document.querySelector(".shows");
+setInterval(()=>{
+    let randomword = Math.floor(Math.random() * words.length);
+    // console.log(words[randomword]);
+    ins.innerHTML ="Have a "+ words[randomword] +" day!";
+}, 100);
