6db19f86239a1c5dd172d34d3103fe1da97cce22
Placide <75864037+PlacideNdai@users.noreply.github.com>
2023-12-19 19:48:51 -0600

    Profile update
    just some improv...

--- app.js
+++ app.js
@@ -1,46 +1,106 @@
-fetch("text.json")
-.then((res)=> res.json())
-.then( (data) =>{
-    $('#aboutme').html(data.aboutme);
-})
+// starting..
 
-// on hover the links
-let con, others,sites, contacts,prop,hobi;
-con = document.querySelector('.con');
-others = document.querySelector('.othersi');
-sites = document.querySelector('.sites');
-contacts = document.querySelector('.contact');
-prop = document.querySelector('.prop');
-hobi  = document.querySelector('.hobi');
+// variable groups
+let bio = document.getElementById("bios");
+let hobbies = document.getElementById("hobbies");
+console.log("Js starting...");
 
-// firsts
-prop.addEventListener('mouseover',()=>{
-    hobi.style.display = 'block';
-    sites.style.display ='none'
-    contacts.style.display = 'none';
-})
-hobi.addEventListener('mouseout',()=>{
-    setTimeout(()=>{hobi.style.display = 'none'},3000)
-})
+let theBio;
+let data_details = null;
+function Bio() {
+    // fetch for the file
+    fetch("./info.json")
+        .then((res) => res.json())
+        .then((data) => {
+            bio.innerHTML = data.Aboutme;
+            theBio = data.Aboutme;
+            data_details = data;
+            let cutts = theBio.split("<br>");
+            cutts.forEach(element => {
+                let changedBIO = element.replace("<br>", "");
+                // testing(changedBIO);
+            });
+        });
+        console.log("Done");
 
-// second
-others.addEventListener('mouseover',()=>{
-    sites.style.display = 'block';
-    hobi.style.display ='none'
-    contacts.style.display = 'none';
-})
-sites.addEventListener('mouseout',()=>{
-    setTimeout(()=>{sites.style.display = 'none'},3000)
-})
+}
+// when the window or tab reload
+window.onload = ()=>{
+    Bio();
+}
 
-// third
-con.addEventListener('mouseover',()=>{
-    contacts.style.display = 'block';
-    hobi.style.display ='none';
-    sites.style.display ='none'
-})
-contacts.addEventListener('mouseout',()=>{
-    setTimeout(()=>{contacts.style.display = 'none'},3000)
-})
+let allHover = document.querySelectorAll(".commonhead");
+
+allHover.forEach(element => {
+    element.addEventListener('mouseover', (event) => {
+        if (element == allHover[0]) {
+            // reload or replace the text with the bio
+            Bio();
+        } else if (element == allHover[1]) {
+            console.log("hobbies");
+            // hobbies
+            hobbies_fun(bio);
+        } else if (element == allHover[2]) {
+            console.log("quick links");
+            // qiock links to other sites
+            quicklinks_fun(bio);
+        } else if (element == allHover[3]) {
+            console.log("contants");
+            // contants
+            contacts_fun(bio);
+        } else if (element == allHover[4]) {
+            console.log("yeah!");
+            // yaeh which should be random stuff
+            yeah_fun(bio);
+        }
+    });
+});
 
 
+// if I need the out event
+allHover.forEach(element => {
+    element.addEventListener('mouseout', (out) => {
+
+    })
+});
+
+
+function quicklinks_fun(bio){
+    bio.innerHTML = "";
+    bio.innerHTML = data_details.quicklinks;
+}
+
+function hobbies_fun(bio){
+    bio.innerHTML = data_details.hobbies;
+}
+
+function yeah_fun(bio){
+    bio.innerHTML = data_details.yeah;
+}
+
+function contacts_fun(bio){
+    bio.innerHTML = data_details.contacts;
+    messageConsole(bio);
+}
+/// ignore
+function testing(readBio) {
+    if ('speechSynthesis' in window) {
+        let message = new SpeechSynthesisUtterance();
+        let voices = speechSynthesis.getVoices();
+        message.text = readBio;
+
+        // finding voices
+        let englishVoices = voices.find(function (voice) {
+            return (voice.lang.startsWith('en') && voice.gender.toLocaleLowerCase().includes('male'));
+        });
+
+        message.voice = englishVoices;
+        speechSynthesis.speak(message);
+    } else {
+        alert("Sorry!");
+    }
+}
+
+function messageConsole(mess){
+    console.log(mess);
+}
