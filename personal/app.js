1026c0aafa3ce996e1e2dfdba36bb8e376dcb079
placidendai <placidendaisenga@gmail.com>
2024-03-07 19:13:04 -0600

    new looks

---
+++ personal/app.js
@@ -0,0 +1,15 @@
+function homeaway(){
+    window.location.href = "../personal/";
+}
+
+function justhome(){
+    window.location.href = "./index.html";
+}
+
+function fivver(){
+    window.location.href = "https://www.fiverr.com/notplacide?"
+}
+
+function freelancer(){
+    window.location.href = "https://www.freelancer.com/u/notplacide?frm=notplacide&sb=t"
+}
